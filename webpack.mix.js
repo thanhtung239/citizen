9cf6c7c9a547a21b95c82d4719d7183ec372517a
hapo-tunght <tung.ht@haposoft.com>
2021-12-21 21:43:13 +0700

    create base front-end

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('resources/assets/img', 'public/img')
+    .copy('resources/assets/fonts', 'public/fonts');
